18cd618d5c07a5610095bd8f79c7a027db4298f2
Corinne Winthrop <corinne.winthrop@gmail.com>
2016-05-02 17:49:07 -0600

    Make purchased list sortable

--- solutions.js
+++ solutions.js
@@ -1,75 +1,76 @@
 $(document).ready(function() {
 
-  var groceries = [
-  {name: "Tomatoes", status: "needed", price: "3.99", quantity: 5},
-  {name: "Onions", status: "needed", price: "1.85", quantity: 2},
-  {name: "Cilantro", status: "needed", price: ".95", quantity: 1},
-  {name: "Limes", status: "complete", price: ".33", quantity: 3},
-  {name: "Jalapeno", status: "complete", price: ".15", quantity: 2}
-  ];
+	var groceries = [
+	{name: "Tomatoes", status: "needed", price: "3.99", quantity: 5},
+	{name: "Onions", status: "needed", price: "1.85", quantity: 2},
+	{name: "Cilantro", status: "needed", price: ".95", quantity: 1},
+	{name: "Limes", status: "complete", price: ".33", quantity: 3},
+	{name: "Jalapeno", status: "complete", price: ".15", quantity: 2}
+	];
 
-  for(i=0; i<groceries.length; i++){
-    var item = groceries[i]
-    if(groceries[i].status == 'needed'){
-      $("#list").append($("<li>" + item.name + " (" + item.quantity + ") @ $" + item.price + "</li>"))
-    }else{
-      $("#purchased").append($("<li>" + item.name + " (" + item.quantity + ") @ $" + item.price + "</li>"))
-    }
-  }
+	for(i=0; i<groceries.length; i++){
+			var item = groceries[i]
+			if(groceries[i].status == 'needed'){
+					$("#list").append($("<li>" + item.name + " (" + item.quantity + ") @ $" + item.price + "</li>"))
+			}else{
+					$("#purchased").append($("<li>" + item.name + " (" + item.quantity + ") @ $" + item.price + "</li>"))
+			}
+	}
 
-  function displayTotal(){
-    var total = 0;
-    for(i=0; i<groceries.length; i++){
-      total += (groceries[i].quantity * groceries[i].price)
-    }
-    $(".totalCost h4 span").text("$" + total.toFixed(2))
-  }
+	function displayTotal(){
+			var total = 0;
+			for(i=0; i<groceries.length; i++){
+					total += (groceries[i].quantity * groceries[i].price)
+			}
+			$(".totalCost h4 span").text("$" + total.toFixed(2))
+	}
 
-  displayTotal();
+	displayTotal();
 
-  $(".btn-success").click(function(){
-    var nameInput = $("#addItem").val();
-    var priceInput = $("#addPrice").val();
-    var quantityInput = $("#addQuantity").val();
-    if (nameInput == '' || priceInput == '' || quantityInput == ''){
-      alert("Please fill in all fields")
-    }
-    else {
-      addItem(nameInput, priceInput, quantityInput);
-    }
-  });
+	$(".btn-success").click(function(){
+			var nameInput = $("#addItem").val();
+			var priceInput = $("#addPrice").val();
+			var quantityInput = $("#addQuantity").val();
+			if (nameInput == '' || priceInput == '' || quantityInput == ''){
+					alert("Please fill in all fields")
+			}
+			else {
+					addItem(nameInput, priceInput, quantityInput);
+			}
+	});
 
-  function addItem(name, price, quantity){
-    groceries.unshift({name: name, status: "needed", price: price, quantity: quantity});
-    updateList();
-  };
+	function addItem(name, price, quantity){
+			groceries.unshift({name: name, status: "needed", price: price, quantity: quantity});
+			updateList();
+	};
 
-  function updateList(){
-    $('#list').empty();
-    $('#purchased').empty();
-    for(i=0; i<groceries.length; i++){
-      var item = groceries[i]
-      if(groceries[i].status == 'needed'){
-        $("#list").append($("<li>" + item.name + " (" + item.quantity + ") @ $" + item.price + "</li>"))
-      }else{
-        $("#purchased").append($("<li>" + item.name + " (" + item.quantity + ") @ $" + item.price + "</li>"))
-      }
-    }
-    displayTotal();
-  }
+	function updateList(){
+			$('#list').empty();
+			$('#purchased').empty();
+			for(i=0; i<groceries.length; i++){
+					var item = groceries[i]
+					if(groceries[i].status == 'needed'){
+							$("#list").append($("<li>" + item.name + " (" + item.quantity + ") @ $" + item.price + "</li>"))
+					}else{
+							$("#purchased").append($("<li>" + item.name + " (" + item.quantity + ") @ $" + item.price + "</li>"))
+					}
+			}
+			displayTotal();
+	}
 
-  $('.btn-warning').on('click', function(){
-    groceries.shift();
-    updateList();
-  });
+	$('.btn-warning').on('click', function(){
+			groceries.shift();
+			updateList();
+	});
 
-  $('#list').sortable();
+	$('#list').sortable();
+	$('#purchased').sortable();
 
 
-  $('#list').on('dblclick','li',function(){
-    var x = ($(this).index());
-    groceries[x].status = "complete";
-    updateList();
-  });
+	$('#list').on('dblclick','li',function(){
+			var x = ($(this).index());
+			groceries[x].status = "complete";
+			updateList();
+	});
 
 });
